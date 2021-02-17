a0e7bd94c75251886f024cdf68b8cf99907dd666
tanyas27 <>
2021-02-17 21:35:21 +0530

    Merge Sorted Array

---
+++ Merge Sorted Array.js
@@ -0,0 +1,32 @@
+/*************************** Question *******************************/
+
+/*
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+Note:
+
+The number of elements initialized in nums1 and nums2 are m and n respectively.
+You may assume that nums1 has enough space (size that is equal to m + n) to hold additional elements from nums2.
+Example:
+
+Input:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+
+Output: [1,2,2,3,5,6]
+
+*/
+
+/**************************** Implementation *****************************/
+
+var duplicateZeros = function(arr) {
+    var x = arr.length;
+    for(let i=0; i < x; i++){
+        if(arr[i]===0){
+        arr.splice(i,0,0);
+        i++;
+       }  
+    }
+    arr.length = x;  
+return arr;
+};
